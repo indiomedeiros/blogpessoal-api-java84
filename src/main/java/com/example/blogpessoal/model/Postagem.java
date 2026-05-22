97c7e50d9291e483675688d7b92f56a47d0d6d70
Indio <indio.medeiros.dev@gmail.com>
2026-05-22 09:00:43 -0300

    feat: implementa o recurso postagens com listar todas as postagens.

--- src/main/java/com/example/blogpessoal/model/Postagem.java
+++ src/main/java/com/example/blogpessoal/model/Postagem.java
@@ -2,6 +2,8 @@ package com.example.blogpessoal.model;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,9 +20,46 @@ public class Postagem {
 	private Long id;
 
 	@NotBlank(message = "O atributo título é obrigatório!")
-	@Size(min = 5, max = 100, message = "O Atributo título deve conter no mínimo 05 e o máximo ")
+	@Size(min = 5, max = 100, message = "O Atributo título deve conter no mínimo 05 e o máximo 100")
 	private String titulo;
+
+	@NotBlank(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O Atributo texto deve conter no mínimo 10 e o máximo 1000 ")
 	private String texto;
+
+	@UpdateTimestamp
 	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
 
 }
