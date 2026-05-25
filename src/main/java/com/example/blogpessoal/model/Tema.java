9468e438f861e55110049eec1bd665fee3981ca8
Indio <indio.medeiros.dev@gmail.com>
2026-05-25 09:16:59 -0300

    feat: cria a entidade tema mapeado para persistência e validação básica

---
+++ src/main/java/com/example/blogpessoal/model/Tema.java
@@ -0,0 +1,37 @@
+package com.example.blogpessoal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "O atributo descricão é obrigatório!")
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
